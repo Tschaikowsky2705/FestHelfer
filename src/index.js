c9cc5045388036d01b1832cd5e874b1a6a49429e
Tschaikowsky2705 <uwe-baumann@gmx.ch>
2025-06-20 23:09:32 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import FestHelferApp from "./FestHelferApp";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <FestHelferApp />
+  </React.StrictMode>
+);

e78ce5529cc458a2b4c7ebf99408301c4f33ec3b
Tschaikowsky2705 <uwe-baumann@gmx.ch>
2025-06-27 00:26:01 +0200

    Update submit.js

--- public/submit.js
+++ public/submit.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 const supabaseUrl = 'https://eggzzfhqljmijnucnxnq.supabase.co';
-const supabaseKey = 'YOUR_SUPABASE_ANON_KEY';  // ← hier Deinen ANON-KEY einsetzen
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVnZ3p6ZmhxbGptaWpudWNueG5xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA0NDQ4ODgsImV4cCI6MjA2NjAyMDg4OH0.fCOh-A_Z6MzUqmCyE7TL-lT1ApP6hAWi9SHzX_0POC8';  // ← hier Deinen ANON-KEY einsetzen
 const supabase    = createClient(supabaseUrl, supabaseKey);
 
 /** Veranstaltungen laden, nach Datum sortiert */
